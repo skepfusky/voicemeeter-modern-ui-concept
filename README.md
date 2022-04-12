60f12f4ee4dc0a29f845ffbcf0a8453af8ce04e8
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-04-12 23:13:55 +0800

    Let's have fun! ✨
    init Vue 3 + TS + Vite + Electron

---
+++ README.md
@@ -0,0 +1,13 @@
+# Voicemeeter Modern UI Concept
+
+Voicemeeter is great and all, it's used by some studios, streamers, YouTubers - where in the 2020s!
+
+The UI needs to be updated or should I say, _modernized_ from today's standards! This is merely just
+a __concept__ as I've barely added any functionality at the time being as I have no idea how to send
+stuff from Voicemeeter to an Electon app.
+
+## Stack
+
+![Stack](https://skillicons.dev/icons?i=vue,ts,electron)
+
+Bootstraped Vue 3 + TypeScript + Vite project with Electron. [Here's a tutorial how!](https://dev.to/brojenuel/vite-vue-3-electron-5h4o)
---
+++ electron/nice-app.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+const isDev = process.env.IS_DEV == "true" ? true : false;
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../dist/index.html')}`
+  );
+  // Open the DevTools.
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});

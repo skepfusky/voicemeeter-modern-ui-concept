82918f163861cbffa1b6d35e92ee4ef1612ef5d4
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-04-13 11:57:41 +0800

    Testing custom trackbar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import TrackBarItem from './components/TrackBarItem.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <TrackBarItem :value=69 />
+  <TrackBarItem :value=21 />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+*, ::before, ::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
 }
+
 </style>
---
+++ src/components/TrackBarItem.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+defineProps<{ value: number }>();
+</script>
+
+<template>
+  <div id="track-bar">
+    <input class="slider" type="range" v-model="value" min="0" max="100" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#track-bar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  width: 175px;
+  position: relative;
+}
+
+@mixin trackThumb {
+  width: 25px;
+  height: 25px;
+  background: #04aa6d;
+  border-radius: 50%;
+  cursor: pointer;
+  transform: scale(1.85);
+}
+
+.slider {
+  --thumb-hover: #05c982;
+  -webkit-appearance: none;
+  background: #d3d3d3;
+  outline: none;
+  transform: rotateZ(270deg);
+  width: 100%;
+  overflow: visible;
+  border-radius: 3rem;
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    @include trackThumb;
+
+    &:hover {
+      background-color: var(--thumb-hover);
+    }
+  }
+
+  &::-moz-range-thumb {
+    @include trackThumb;
+
+    &:hover {
+      background-color: var(--thumb-hover);
+    }
+  }
+}
+</style>
